b3057cf6c4fd18ed7c52ac6d1fd0dd9e25f735c5
ritiktaneja <ritiktaneja10@gmail.com>
2020-01-22 23:07:45 +0530

    port changed to 8001

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ messagesArray=[{
     'message':''
 }];
 
-server.listen(process.env.PORT || 80);
+server.listen(process.env.PORT || 8001);
 
 console.log('Server running...');
 app.get('/',function(req,res){
